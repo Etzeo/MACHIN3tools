4722a102d1e76fb8809f6f76780df0de3fe374a5
Etzeo <14105275+Etzeo@users.noreply.github.com>
2019-07-01 18:00:03 +1000

    Updated get_addon() with temp bypass

--- utils/registration.py
+++ utils/registration.py
@@ -23,24 +23,25 @@ def get_addon(addon, debug=False):
     # look for addon by name and find folder name and path
     # Note, this will also find addons that aren't registered!
 
-    for mod in addon_utils.modules():
-        name = mod.bl_info["name"]
-        version = mod.bl_info.get("version", None)
-        foldername = mod.__name__
-        path = mod.__file__
-        enabled = addon_utils.check(foldername)[1]
+    #for mod in addon_utils.modules():
+    #    name = mod.bl_info["name"]
+    #    version = mod.bl_info.get("version", None)
+    #    foldername = mod.__name__
+    #    path = mod.__file__
+    #    enabled = addon_utils.check(foldername)[1]
 
-        if name == addon:
-            if debug:
-                print(name)
-                print("  enabled:", enabled)
-                print("  folder name:", foldername)
-                print("  version:", version)
-                print("  path:", path)
-                print()
+    #    if name == addon:
+    #        if debug:
+    #            print(name)
+    #            print("  enabled:", enabled)
+    #            print("  folder name:", foldername)
+    #            print("  version:", version)
+    #            print("  path:", path)
+    #            print()
 
-            return enabled, foldername, version, path
-    return False, None, None, None
+    #        return enabled, foldername, version, path
+    #return False, None, None, None
+    return True, None, None, None   # change 'True' to 'False' if you have none of the following plugins: DECALmachine, Group Pro, BatchOps
 
 
 def get_addon_prefs(addon):
